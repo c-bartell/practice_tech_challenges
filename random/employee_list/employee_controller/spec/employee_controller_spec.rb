b592ac124af631120b58c2b94ea5f97d5c04c52a
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-05-07 15:00:38 -0600

    Add test for all employee names

--- random/employee_list/employee_controller/spec/employee_controller_spec.rb
+++ random/employee_list/employee_controller/spec/employee_controller_spec.rb
@@ -1,4 +1,47 @@
 # frozen_string_literal: true
 
 RSpec.describe EmployeeController do
+  context "a company with several layers of management" do
+    def initialize_company
+      zelda = Employee.new(
+        'Zelda',
+        [
+          Employee.new('Pat')
+        ]
+      )
+      robert = Employee.new(
+        'Robert',
+        [
+          zelda,
+          Employee.new('Anita')
+        ]
+      )
+      todd = Employee.new(
+        'Todd',
+        [
+          Employee.new('Randy'),
+          Employee.new('Tina')
+        ]
+      )
+      
+      return Employee.new(
+        'Yumiko',
+        [
+          Employee.new('Virginia'),
+          robert,
+          todd
+        ]
+      )
+    end
+
+    def ceo
+      @ceo ||= initialize_company
+    end
+
+    it "Can list all employees" do
+      all_names = ['Zelda', 'Pat', 'Robert', 'Anita', 'Todd', 'Randy', 'Tina', 'Yumiko', 'Virginia']
+
+      expect(EmployeeController.list_reports(ceo).sort).to eq(all_names.sort)
+    end
+  end
 end
