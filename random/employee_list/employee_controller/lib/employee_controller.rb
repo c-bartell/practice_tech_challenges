8b26a6cd2ea45ec2f1a69e5bfcdbb5226a6ffb10
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-05-07 15:01:18 -0600

    Add method to return names of all employee reports

--- random/employee_list/employee_controller/lib/employee_controller.rb
+++ random/employee_list/employee_controller/lib/employee_controller.rb
@@ -1,2 +1,16 @@
 class EmployeeController
+  class << self
+    def list_reports(employee)
+      names = []
+      stack = [employee]
+
+      until stack.empty?
+        person = stack.pop
+        names << person.name
+        stack += person.direct_reports if person.direct_reports 
+      end
+
+      return names
+    end
+  end
 end
